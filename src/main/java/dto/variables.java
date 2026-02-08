d17dfbcb8e3d60e3c94e9bba1cee119cc902ad8b
ryantakka <ryan.takka@hotmail.com>
2026-02-07 23:49:58 -0500

    Variables have been Created

---
+++ src/main/java/dto/variables.java
@@ -0,0 +1,76 @@
+package dto;
+
+public class variables {
+    private int days;
+    private double budget;
+    private double flightCost;
+    private double nightCost;
+    private double foodCost;
+    private double activitiesCost;
+    private double transportCost;
+    private double emergencyMoney;
+
+    public int getDays() {
+        return days;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public double getFlightCost() {
+        return flightCost;
+    }
+
+    public double getNightCost() {
+        return nightCost;
+    }
+
+    public double getFoodCost() {
+        return foodCost;
+    }
+
+    public double getActivitiesCost() {
+        return activitiesCost;
+    }
+
+    public double getTransportCost() {
+        return transportCost;
+    }
+
+    public double getEmergencyMoney() {
+        return emergencyMoney;
+    }
+
+    public void setDays(int days) {
+        this.days = days;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public void setFlightCost(double flightCost) {
+        this.flightCost = flightCost;
+    }
+
+    public void setNightCost(double nightCost) {
+        this.nightCost = nightCost;
+    }
+
+    public void setFoodCost(double foodCost) {
+        this.foodCost = foodCost;
+    }
+
+    public void setActivitiesCost(double activitiesCost) {
+        this.activitiesCost = activitiesCost;
+    }
+
+    public void setTransportCost(double transportCost) {
+        this.transportCost = transportCost;
+    }
+
+    public void setEmergencyMoney(double emergencyMoney) {
+        this.emergencyMoney = emergencyMoney;
+    }
+}

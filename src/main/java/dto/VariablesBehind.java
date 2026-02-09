bf9eb9be1947f300ffb2d43d12d17d17d4c3d28a
ryantakka <ryan.takka@hotmail.com>
2026-02-08 21:13:55 -0500

    VariablesBehind.java Completed

--- src/main/java/dto/VariablesBehind.java
+++ src/main/java/dto/VariablesBehind.java
@@ -1,4 +1,55 @@
 package dto;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class VariablesBehind {
+
+    private double budgetGap;
+    private double totalCost;
+    private String VacationStatus;
+
+    private HashMap<String, Double> costDetail;
+    private List<String> suggestions;
+
+    public VariablesBehind (String VacationStatus, double budgetGap, double totalCost, HashMap<String, Double> costDetail,List<String> suggestions){
+        this.VacationStatus = VacationStatus;
+        this.budgetGap = budgetGap;
+        this.totalCost = totalCost;
+        this.costDetail = costDetail;
+        this.suggestions = suggestions;
+    }
+
+    public double getBudgetGap() {
+        return budgetGap;
+    }
+
+    public void setBudgetGap(double budgetGap) {
+        this.budgetGap = budgetGap;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public String getVacationStatus() {
+        return VacationStatus;
+    }
+
+    public void setVacationStatus(String vacationStatus) {
+        VacationStatus = vacationStatus;
+    }
+
+    public HashMap<String, Double> getCostDetail() {
+        return costDetail;
+    }
+
+    public void setCostDetail(HashMap<String, Double> costDetail) {
+        this.costDetail = costDetail;
+    }
 }
